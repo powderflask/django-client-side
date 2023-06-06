e491ab7a4f3e6604426f9f73e5366232ec8f1ac2
Jacob <jacobtumak@gmail.com>
2023-06-05 22:34:07 -0700

    set-up sphinx with markdown-compatibility

---
+++ build/lib/client_side/__init__.py
@@ -0,0 +1,9 @@
+""""
+django-client-side.
+
+Simple client-side dependency management
+"""
+
+__version__ = "0.2.0"
+__author__ = 'powderflask'
+default_app_config = 'client_side.apps.ClientSideConfig'
